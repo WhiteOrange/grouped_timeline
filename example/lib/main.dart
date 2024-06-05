4c049aaa47e9ee65ee9d378780db8c394b5c329d
nileshk-wos <nileshk@whiteorangesoftware.com>
2024-06-05 14:37:51 +0530

    dart format

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,63 +24,111 @@ class MyApp extends StatelessWidget {
         timeLineInfo: [
           TimeLineInfo(
             date: "12 May",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=1", "https://source.unsplash.com/random/200x200?sig=2"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=1",
+              "https://source.unsplash.com/random/200x200?sig=2"
+            ],
           ),
           TimeLineInfo(
             date: "14 May",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=3", "https://source.unsplash.com/random/200x200?sig=4"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=3",
+              "https://source.unsplash.com/random/200x200?sig=4"
+            ],
           ),
           TimeLineInfo(
             date: "12 May",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=5", "https://source.unsplash.com/random/200x200?sig=6"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=5",
+              "https://source.unsplash.com/random/200x200?sig=6"
+            ],
           ),
           TimeLineInfo(
             date: "24 Jan",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=7", "https://source.unsplash.com/random/200x200?sig=8"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=7",
+              "https://source.unsplash.com/random/200x200?sig=8"
+            ],
           ),
           TimeLineInfo(
             date: "24 Jan",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=9", "https://source.unsplash.com/random/200x200?sig=10"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=9",
+              "https://source.unsplash.com/random/200x200?sig=10"
+            ],
           ),
           TimeLineInfo(
             date: "13 May",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=11", "https://source.unsplash.com/random/200x200?sig=12"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=11",
+              "https://source.unsplash.com/random/200x200?sig=12"
+            ],
           ),
           TimeLineInfo(
             date: "13 May",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=13", "https://source.unsplash.com/random/200x200?sig=14"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=13",
+              "https://source.unsplash.com/random/200x200?sig=14"
+            ],
           ),
           TimeLineInfo(
             date: "13 May",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=15", "https://source.unsplash.com/random/200x200?sig=16"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=15",
+              "https://source.unsplash.com/random/200x200?sig=16"
+            ],
           ),
           TimeLineInfo(
             date: "14 May",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=17", "https://source.unsplash.com/random/200x200?sig=18"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=17",
+              "https://source.unsplash.com/random/200x200?sig=18"
+            ],
           ),
           TimeLineInfo(
             date: "14 May",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=19", "https://source.unsplash.com/random/200x200?sig=20"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=19",
+              "https://source.unsplash.com/random/200x200?sig=20"
+            ],
           ),
           TimeLineInfo(
             date: "11 Feb",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=21", "https://source.unsplash.com/random/200x200?sig=22"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=21",
+              "https://source.unsplash.com/random/200x200?sig=22"
+            ],
           ),
           TimeLineInfo(
             date: "11 Feb",
-            title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
-            images: ["https://source.unsplash.com/random/200x200?sig=23", "https://source.unsplash.com/random/200x200?sig=24"],
+            title:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin eu orci at bibendum. Etiam semper volutpat magna, a vehicula",
+            images: [
+              "https://source.unsplash.com/random/200x200?sig=23",
+              "https://source.unsplash.com/random/200x200?sig=24"
+            ],
           ),
         ],
       ),
